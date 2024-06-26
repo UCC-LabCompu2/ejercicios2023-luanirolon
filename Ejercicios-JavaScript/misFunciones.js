ca1fb7b280267fef2b5f1c3e2cdaba21eb979352
Luana <128556154+luanirolon@users.noreply.github.com>
2024-06-26 17:59:53 -0300

    Update misFunciones.js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -115,5 +115,27 @@ function dibujarCirCuad() {
     ctx.stroke();
     ctx.fill();
 }
+var bandera;
 
+function dibujar(event) {
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX, posY);
+
+    canvas.onmousedown = function() { bandera = true; };
+    canvas.onmouseup = function() { bandera = false; };
+
+    if (bandera) {
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill();
+    }
+}
+function limpiarCanvas(){
+  var canvas= document.getElementById("canvasAdibujar");
+  var ctx= canva.getContext("2d");
+  canvas.widht = canvas.widht;
+}
 
